362abf9a61e808fcb1a1f6bb1f4ac2f0ccd0b160
Shane Schilling <trilambda122@gmail.com>
2021-06-24 18:26:25 -0500

    add record form. added toogle switches

--- src/components/AddRecordForm.js
+++ src/components/AddRecordForm.js
@@ -1,62 +1,150 @@
-import React from "react";
+import React, { useState } from "react";
+import BootstrapSwitchButton from "bootstrap-switch-button-react";
 
 export default function AddRecordForm() {
+  //Set State
+  const [exerciseToogle, setExerciseToogle] = useState();
+  const [kindnessToogle, setKindnessToogle] = useState();
 
-   
-    const rangeBarmove = ()=>{
-        console.log('moving the bar')
-        const barValue=document.getElementById('sleep-hours').value
-        console.log(document.getElementById('sleep-hours').value)
-        document.getElementById('sleep-hour-value').innerHTML = barValue
-    }
+  // Functions
+
+  // toogle buttons
+  const toogleExercise = () => setExerciseToogle(!exerciseToogle);
+  const toogleKindness = () => setKindnessToogle(!kindnessToogle);
+
+  // range bars
+  const rangeBarmove = () => {
+    const barValue = document.getElementById("sleep-hours").value;
+    document.getElementById("sleep-hour-value").innerHTML = barValue;
+  };
+
+  const rangeHappyScoremove = () => {
+    const barValue = document.getElementById("happy-score").value;
+    document.getElementById("happy-score-value").innerHTML = barValue;
+  };
+
   return (
     <div>
       <form className="needs-validation" id="happyForm" novalidate>
         <div className="row g-3">
+          {/* Range bars */}
           <div className="col-sm-6">
-            <label for="happyScore" className="form-label">
+            <label for="happy-score" className="form-label">
               Happy Score
             </label>
-            <select
-              className="form-select"
-              id="happyScore"
-              aria-label="HappyScore"
-            >
-              <option selected>Happy Score</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
+
+            <input
+              onChange={rangeHappyScoremove}
+              type="range"
+              className="form-range"
+              min="0"
+              max="5"
+              step="1"
+              id="happy-score"
+            ></input>
+            <span id="happy-score-value" className="badge bg-secondary">
+              --
+            </span>
           </div>
 
           <div className="col-sm-6">
             <label for="sleep-hours" className="form-label">
               Sleep Hours
             </label>
-            
-            <input onChange={rangeBarmove}
+
+            <input
+              onChange={rangeBarmove}
               type="range"
-              class="form-range"
+              className="form-range"
               min="0"
               max="24"
               step="1"
               id="sleep-hours"
-            
             ></input>
-            <span id="sleep-hour-value" class="badge bg-secondary">0</span>
+            <span id="sleep-hour-value" className="badge bg-secondary">
+              --
+            </span>
           </div>
-          <hr className="my-4" />
 
-          <div className="row g-3">
-            <label for="happyNote" className="form-label">
-              Kindness Note
-            </label>
-            <textarea name="happyNote" form="happyForm">
-              Enter text here...
-            </textarea>
+          {/* Toogle switches */}
+          <div className="container">
+            <div className="col-sm-3">
+              <BootstrapSwitchButton
+                id="exercise-switch"
+                checked={toogleExercise}
+                size="sm"
+                onChange={toogleExercise}
+              />
+              <p className="form-label">Exercise</p>
+            </div>
+            {exerciseToogle ? (
+              <div className='col-sm-10 m-3'>
+                <label className="form-check-label me-3" >Exercise Level</label>
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="low"
+                    value="low"
+                  />
+                  <label className="form-check-label" for="inlineRadio1">
+                    Low
+                  </label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="med"
+                    value="med"
+                  />
+                  <label className="form-check-label" for="inlineRadio2">
+                    Med
+                  </label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="high"
+                    value="high"
+                    
+                  />
+                  <label className="form-check-label" for="inlineRadio3">
+                    High
+                  </label>
+                </div>
+              </div>
+            ) : (
+              <h1></h1>
+            )}
+
+            <div className="col-sm-3">
+              <BootstrapSwitchButton
+                id="kindness-switch"
+                checked={toogleKindness}
+                size="sm"
+                onChange={toogleKindness}
+              />
+              <p className="form-label">Kindness</p>
+            </div>
           </div>
+          {kindnessToogle ? (
+            <div className="row g-3">
+              <hr className="my-4" />
+              <label for="gratNote" className="form-label">
+                Kindness Note
+              </label>
+              <textarea name="gratNote" form="happyForm">
+                Enter text here...
+              </textarea>
+            </div>
+          ) : (
+            <h1></h1>
+          )}
 
           <hr className="my-4" />
           <div className="row g-3">
@@ -68,7 +156,7 @@ export default function AddRecordForm() {
             </textarea>
           </div>
           <hr className="my-4" />
-          <button className="w-50 btn btn-primary btn-lg" type="submit">
+          <button className=" col-sm-4  btn btn-primary btn-lg" type="submit">
             Submit
           </button>
         </div>
